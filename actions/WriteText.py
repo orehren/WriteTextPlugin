52a5657943e86781d41b324b0732248a56ba4a51
orehren <o.rehren@gmail.com>
2024-12-16 12:04:30 +0100

    change WriteText.py

--- actions/WriteText.py
+++ actions/WriteText.py
@@ -45,10 +45,10 @@ class WriteText(ActionBase):
             self.xkb_keymap = None
             self.xkb_state = None
     
-   def _get_evdev_keycodes(self, text: str) -> List[int]:
+    def _get_evdev_keycodes(self, text: str) -> List[int]:
         if not self.xkb_state or not self.xkb_keymap:
-             log.error("xkbcommon is not set up correctly.")
-             return []
+            log.error("xkbcommon is not set up correctly.")
+            return []
 
         keycodes = []
         for char in text:
