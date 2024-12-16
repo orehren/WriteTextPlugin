76f1e3cb4cfaa375c854f8622f326b4df9feef01
orehren <o.rehren@gmail.com>
2024-12-16 09:57:05 +0100

    change WriteText.py

--- actions/WriteText.py
+++ actions/WriteText.py
@@ -36,7 +36,8 @@ class WriteText(ActionBase):
     def _setup_xkb(self):
         try:
             self.xkb_context = xkb.Context()
-            self.xkb_keymap = xkb.Keymap.from_string(self.xkb_context, "default", xkb.KEYMAP_FORMAT_TEXT_V1, xkb.KEYMAP_COMPILE_NO_FLAGS)
+            keymap_str = xkb.Keymap.keymap_from_name(self.xkb_context, "default", xkb.KEYMAP_FORMAT_TEXT_V1)
+            self.xkb_keymap = xkb.Keymap.from_string(self.xkb_context, keymap_str, xkb.KEYMAP_FORMAT_TEXT_V1, xkb.KEYMAP_COMPILE_NO_FLAGS)
             self.xkb_state = xkb.State(self.xkb_keymap)
             log.debug("xkbcommon setup successful")
         except Exception as e:
