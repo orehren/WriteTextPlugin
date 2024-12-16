6cf681bbfb4146f05700f3d74628d889212cf938
orehren <o.rehren@gmail.com>
2024-12-16 12:58:23 +0100

    Edit WriteText.py

--- actions/WriteText.py
+++ actions/WriteText.py
@@ -55,27 +55,30 @@ class WriteText(ActionBase):
         if not self.xkb_state or not self.xkb_keymap:
              log.error("xkbcommon is not set up correctly.")
              return []
+        
+        log.debug(f"xkb Attributes: {dir(xkb)}")
+        log.debug(f"xkb Attributes: {dir(xkb_symbols)}")
 
         keycodes = []
         for char in text:
             utf32_char = ord(char)
+            
             found_keycodes = []
             for keycode in self.xkb_keymap:
                 symbols = self.xkb_keymap.key_get_syms_by_level(keycode, 0, 0)
-                log.debug(f"Context Attributes: {dir(self.xkb_keymap.key_get_syms_by_level)}")
                 if not symbols:
                     continue
 
                 for symbol in symbols:
-                    if xkb.keysym_to_utf32(symbol) == utf32_char:
-                        found_keycodes.append(keycode)
-                        break
+                    #if xkb.keysym_to_utf32(symbol) == utf32_char: # Removed for now, as we need to check the available methods
+                    #    found_keycodes.append(keycode)
+                    #    break
+                  pass
             
             if not found_keycodes:
                 log.warning(f"No keycode found for character: {char} (UTF-32: {utf32_char})")
             else:
                 keycodes.extend(found_keycodes)
-        
         return keycodes
 
     def get_custom_config_area(self):
